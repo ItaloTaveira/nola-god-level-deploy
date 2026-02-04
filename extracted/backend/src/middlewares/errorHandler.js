a7a414b42c77ce6f31a9c8990e2f2f6f2a69b186
Italo Taveira <italotaveira@outlook.com>
2026-02-04 12:21:44 -0300

    ..

--- extracted/backend/src/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const logger = require('../utils/logger');
-
-function errorHandler(err, req, res, next) {
-  logger.error(err.message || String(err));
-  res.status(500).json({ ok: false, error: err.message || 'Internal Error' });
-}
-
-module.exports = errorHandler;
