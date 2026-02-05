c6fb447b0f87a1b02ebca78e828f6ecf04694778
ItaloTaveira <italotaveira@outlook.com>
2026-02-05 15:43:00 -0300

    ..

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+// Registra plugins globais do Chart.js antes de carregar a árvore do app
+import './lib/chartSetup'
 import App from './App'
 import './styles.css'
-import './lib/chartSetup'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
