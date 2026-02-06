f6ab3683fff3d01e750d867321fc7156a01b102a
ItaloTaveira <italotaveira@outlook.com>
2026-02-06 17:51:56 -0300

    ..

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
-// Import único do Chart.js para evitar múltiplas instâncias
-import 'chart.js/auto'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
